993111c8ca985eedd8fd033dafbba3bca383702c
hernandohack <56355411+hernandohack@users.noreply.github.com>
2024-05-17 20:48:51 -0500

    nueva clase
    Se creo la clase nueva estudiante, que hereda de persona,

---
+++ src/modelos/Estudiante.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author Hernando Moran
+ */
+public class Estudiante {
+    
+    private double calificacion;
+    
+}
--- src/practica/Practica.java
+++ src/practica/Practica.java
@@ -24,6 +24,7 @@ public class Practica {
         e1.setSueldo(2500);
         
         Empleado e2 = new Empleado();
+        
         e2.getDocumento();
        
     }
